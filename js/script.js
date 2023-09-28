aefa3c2f15d6840be70bc50432626e229d8f0e7f
Agustin Mancini Bobadilla <d48070622@instituto.itr.edu.ar>
2023-09-28 10:06:11 -0300

    botones

--- js/script.js
+++ js/script.js
@@ -1,10 +1,51 @@
-let figuras = ['X', 'O'];
+//anuncio inicio 
+const swalWithBootstrapButtons = Swal.mixin({
+  customClass: {
+    confirmButton: 'btn btn-success',
+    cancelButton: 'btn btn-danger'
+  },
+  buttonsStyling: false
+})
+swalWithBootstrapButtons.fire({
+  background: '#fff url(https://sweetalert2.github.io/images/trees.png)',
+title: '¿Jugador 1, qué queres ser?',
+text: "Seleccione una opcion",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonText: 'Exclamacion',
+  cancelButtonText: 'Cruz',
+  reverseButtons: true
+}).then((result) => {
+  if (result.isConfirmed) {
+    swalWithBootstrapButtons.fire(
+      'jugador 1',
+      'Has elegido exclamacion',
+      'warning',
+    )
+  } else if (
+    /* Read more about handling dismissals below */
+    result.dismiss === Swal.DismissReason.cancel
+  ) {
+    swalWithBootstrapButtons.fire(
+      'Jugador 1',
+      'Has elegido cruz',
+      'error'
+    )
+  }
+})
+
+let figuras = ['X', '!'];
+let lugar ;
 let inicial = 0;
+let celda=[1,2,3,4,5,6,7,8,9];
+while(indice<=celda.length){
+  let indice=0;
+  lugar = document.getElementById(celda[indice]); 
 
-Swal.fire('Bienvenidos a Tateti 2.0');
-
+}
+let indice=0; 
 //Recuperar la primer celda
-const celda1 = document.getElementById('uno');
+
 
 //Agregar a esa celda un evento de escucha click
 celda1.addEventListener('click', () => {
