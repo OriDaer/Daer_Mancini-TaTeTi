bdd118c1206a1cbed9ce3256fa714021a1c7416e
Agustin Mancini Bobadilla <d48070622@instituto.itr.edu.ar>
2023-10-05 09:55:06 -0300

    puntos x error reinicio

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ text: "Seleccione una opcion",
       'warning',
     )
     let figuras = ['X', '!'];
-    let index = 1;
+    let indice = 1;
     let lugar = [null, null, null, null, null, null, null, null, null]; // Array del lugar
     let ganador=false;
 
@@ -50,9 +50,9 @@ text: "Seleccione una opcion",
         let celda = document.getElementById(val);
         celda.addEventListener('click', () => {
             if (!ganador && lugar[val - 1] === null) { // Verificar si la celda está vacía antes de asignar un valor
-                lugar[val - 1] = figuras[index]; // Actualizar el estado del lugar con el símbolo actual
-                celda.innerHTML = figuras[index]; // Establecer el contenido de la celda como la figura actual
-                index = (index + 1) % figuras.length; // Alterna 'X' y '!'
+                lugar[val - 1] = figuras[indice]; // Actualizar el estado del lugar con el símbolo actual
+                celda.innerHTML = figuras[indice]; // Establecer el contenido de la celda como la figura actual
+                indice = (indice + 1) % figuras.length; // Alterna 'X' y '!'
                 verifGanador();
             }
         });
@@ -67,7 +67,8 @@ text: "Seleccione una opcion",
     let figuras = ['X', '!'];
     let index = 0;
     let lugar = [null, null, null, null, null, null, null, null, null]; // Array del lugar
-    let ganador=false;
+    let ganadorX=false;
+    let puntosX= 0;
     function verifGanador() {
         const ganar = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8], // filas
@@ -78,23 +79,30 @@ text: "Seleccione una opcion",
         for (let i = 0; i < ganar.length; i++) {
             const [u, d, t] = ganar[i];
             if (lugar[u] && lugar[u] === lugar[d] && lugar[u] === lugar[t]) {
-                console.log('ganaste');
-                ganador=true;
-                break;
+                console.log('ganasteX');
+                ganadorX=true;
+                if(ganadorX === true){
+                  puntosX ++;
+                  console.log(puntosX);
+                }
+                
             }
+
         }
     }
     
     for (let val = 1; val <= 9; val++) {
         let celda = document.getElementById(val);
         celda.addEventListener('click', () => {
-            if (!ganador && lugar[val - 1] === null) { // Verificar si la celda está vacía antes de asignar un valor
+            if (!ganadorX && lugar[val - 1] === null) { // Verificar si la celda está vacía antes de asignar un valor
                 lugar[val - 1] = figuras[index]; // Actualizar el estado del lugar con el símbolo actual
                 celda.innerHTML = figuras[index]; // Establecer el contenido de la celda como la figura actual
                 index = (index + 1) % figuras.length; // Alterna 'X' y '!'
                 verifGanador();
-            }
-        });
+                lugar = [null, null, null, null, null, null, null, null, null];
+                celda.innerHTML=null; 
+            } 
+        });break;
     }
   }
 });
